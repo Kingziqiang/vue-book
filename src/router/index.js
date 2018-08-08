87e5df6e50a46764db62daf31e43f454152b961b
Kingziqiang <1598971729@qq.com>
2018-08-08 23:23:41 +0800

    分割代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router);
-import Home  from '../components/home.vue';
-import List  from '../components/list.vue';
-import Detail  from '../components/detail.vue';
-import Add  from '../components/add.vue';
-import Collect  from '../components/collect.vue';
+//改用路由懒加载
+// import Home  from '../components/home.vue';
+// import List  from '../components/list.vue';
+// import Detail  from '../components/detail.vue';
+// import Add  from '../components/add.vue';
+// import Collect  from '../components/collect.vue';
 
 export default new Router({
   routes: [
@@ -13,14 +14,25 @@ export default new Router({
     //添加路由元信息
     {
       path:'/home',
-      component:Home,
+      component:()=>import('../components/home.vue'),
       meta:{keepAlive:true}//设置缓存，默认不缓存
       },
-    {path:'/list',component:List},
-    {path:'/add',component:Add},
-    {path:'/detail/:did',component:Detail,name:'detail'},
-    {path:'/collect',component:Collect},
-    {path:'*',redirect:'/home'}
-
+    {
+      path:'/list',
+      component:()=>import('../components/list.vue'),
+    },
+    {path:'/add',
+      component:()=>import('../components/add.vue'),
+    },
+    {path:'/detail/:did',
+      component:()=>import('../components/detail.vue'),
+      name:'detail'
+    },
+    {path:'/collect',
+      component:()=>import('../components/collect.vue'),
+    },
+    {path:'*',
+      redirect:'/home'
+    }
   ]
 })
